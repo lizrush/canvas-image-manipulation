abeb80bd16a135654d268d509f9aec4ca9564705
liz rush <liz@lizmrush.com>
2017-07-25 20:34:55 -0700

    Draw image in canvas

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,9 @@
-// This is where it all goes :)
+window.onload = function(){
+  var img    = document.getElementById('palmtree'),
+      canvas = document.getElementById('canvas'),
+      ctx    = canvas.getContext('2d');
+
+  canvas.width = img.width;
+  canvas.height = img.height;
+  ctx.drawImage(img, 0, 0, img.width, img.height);
+};
