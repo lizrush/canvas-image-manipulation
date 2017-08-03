e9767fbdd935e1159ceb9c0eaa3f7bb3dc2b1d9b
liz rush <liz@lizmrush.com>
2017-08-02 17:06:30 -0700

    Move gratient into its own function

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -24,24 +24,32 @@ window.onload = function(){
       data[p] = data[p+1] = data[p+2] = avg;
   }
 
-  // Create a gradient from which to pick colors from the duotone
-  // based on pixel luminence.
-  var gradientArray = [];
   // Color as array for rgb values
+  // TODO: generate this array from an rgba color
+  // to allow more flexibility on input here
   var green = [80, 254, 16];
   var red = [254, 16, 16];
 
-  // arbitrarily choosing a gradient of 255 values for each
-  for (var d = 0; d < 255; d += 1) {
-    var ratio = d / 255;
-    var l = ratio;
-    var rA = Math.floor(green[0] * l + green[0] * (1 - l));
-    var gA = Math.floor(green[1] * l + green[1] * (1 - l));
-    var bA = Math.floor(green[2] * l + green[2] * (1 - l));
-    gradientArray.push([rA, gA, bA]);
-  }
+  // Create a gradient from which to pick colors from the duotone
+  // based on pixel luminence.
+  var gradientArray = createGradient(green, red);
 
   console.log(gradientArray)
 
   ctx.putImageData(imageData, 0, 0);
 };
+
+function createGradient(color1, color2) {
+  var gradient = [];
+
+  for (i = 0; i < 255; i += 1) {
+    var ratio = i / 255;
+    var l = ratio;
+    var r = Math.floor(color1[0] * l + color2[0] * (1 - l));
+    var g = Math.floor(color1[1] * l + color2[1] * (1 - l));
+    var b = Math.floor(color1[2] * l + color2[2] * (1 - l));
+    gradient.push([r, g, b]);
+  }
+
+  return gradient;
+}
